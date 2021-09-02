4d88ed39517566744e35f5eda8eff1fffdebae51
Fenikz31 <fenikz31@gmail.com>
2021-09-02 12:25:00 +0200

    Environment variables management.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const Express = require('express'),
-      Mysql = require('mysql');
+      Mysql = require('mysql'),
+      dotenv = require('dotenv').config();
 
-const app = Express()
+const app = Express(),
+      { PORT } = process.env
+
 
 app.get( '/', ( req, res ) => res.send( '<h1>Hello World from GET!</h1>' ))
 app.delete( '/', ( req, res ) => res.send( '<h1>Hello World from DELETE!</h1>' ))
 app.post( '/', ( req, res ) => res.send( '<h1>Hello World from POST!</h1>' ))
 app.patch( '/', ( req, res ) => res.send( '<h1>Hello World from PATCH!</h1>' ))
 
-app.listen(4000, () => console.log( `App running on port 4000` ))
+app.listen( PORT, () => console.log( 'App running on port ' + PORT ))
+// app.listen( PORT, () => console.log( `App running on port ${ PORT }` ))
