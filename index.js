c6167c5f7aca054731c7ddad61596c38c636ec42
Fenikz31 <fenikz31@gmail.com>
2021-09-02 12:45:07 +0200

    Database connection.

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ const Express = require('express'),
       dotenv = require('dotenv').config();
 
 const app = Express(),
-      { PORT } = process.env
+      { MYSQL_PASSWORD, MYSQL_USER, PORT } = process.env
+
+const connection = Mysql.createConnection({
+  host: 'localhost',
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  port: 3308
+});
+
+connection.connect( (err) => {
+  if ( err ) {
+    return console.log( 'err: ', err);
+  }
+
+  console.log( 'connected');
+});
 
 
 app.get( '/', ( req, res ) => res.send( '<h1>Hello World from GET!</h1>' ))
@@ -11,5 +26,4 @@ app.delete( '/', ( req, res ) => res.send( '<h1>Hello World from DELETE!</h1>' )
 app.post( '/', ( req, res ) => res.send( '<h1>Hello World from POST!</h1>' ))
 app.patch( '/', ( req, res ) => res.send( '<h1>Hello World from PATCH!</h1>' ))
 
-app.listen( PORT, () => console.log( 'App running on port ' + PORT ))
-// app.listen( PORT, () => console.log( `App running on port ${ PORT }` ))
+app.listen( PORT, () => console.log( `App running on port ${ PORT }` ))
