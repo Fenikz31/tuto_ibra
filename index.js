aa9b525ef249c1d92254be5ca12ed95321504da5
Fenikz31 <fenikz31@gmail.com>
2021-09-02 12:04:58 +0200

    Database creation with docker.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const Express = require('express'),
 
 const app = Express()
 
-app.get( '/', ( req, res ) => res.send( '<h1>Hello World!</h1>' ))
+app.get( '/', ( req, res ) => res.send( '<h1>Hello World from GET!</h1>' ))
+app.delete( '/', ( req, res ) => res.send( '<h1>Hello World from DELETE!</h1>' ))
+app.post( '/', ( req, res ) => res.send( '<h1>Hello World from POST!</h1>' ))
+app.patch( '/', ( req, res ) => res.send( '<h1>Hello World from PATCH!</h1>' ))
 
 app.listen(4000, () => console.log( `App running on port 4000` ))
